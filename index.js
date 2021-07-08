56a9fe6df42b206ff391eedaf072fb8b470b0b03
sluongo718 <sluongo718@yahoo.com>
2021-07-08 14:58:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+function createEmployeeRecord(employee) {
+    return { firstName: employee[0],
+            familyName: employee[1],
+            title: employee[2],
+            payPerHour: employee[3],
+            timeInEvents: [],
+            timeOutEvents: []
+   }
+}
+
+function createEmployeeRecords(arr) {
+    return arr.map(ar => createEmployeeRecord(ar))
+}
+
+function createTimeInEvent(){
+  return timeInEvents
+}
